b97105fc6f54739665ec9bf7398fd8612fad011e
Stephen Barr <stephen@ip-192-168-151-120.ec2.internal>
2022-04-20 11:10:37 -0700

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import boto3
+import uuid
+
+client = boto3.client(
+    's3',
+    aws_access_key_id="AKIARVYAQ4UKQFZXXHHE",
+    aws_secret_access_key="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",
+)
+
+picture = st.camera_input("Take a picture")
+s3bucket = "sales-calls-df-demo"
+
+st.write(f"Take a picture!")
+
+if picture:
+    ukey = str(uuid.uuid4())
+    s3key = "inboundimages/" + ukey + ".jpg"
+    fname = "/tmp/" + ukey + ".jpg"
+    ## Write image to temp file
+    f = open(fname, 'wb')
+    f.write(picture.getvalue())
+    f.close()
+
+    ## Show the user, and tell them what happened
+    st.image(picture)
+
+    ## Upload the file
+    client.upload_file(fname, s3bucket, s3key)
+    st.write(f"Uploaded {s3key} to {s3bucket}")
+
